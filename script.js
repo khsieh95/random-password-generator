7df835d0ca5be6fb59dc9df84ab4f51e097c81e4
Kevin Hsieh <khsieh95@gmail.com>
2020-12-09 09:55:34 -0800

    added variables and while loop

--- script.js
+++ script.js
@@ -12,21 +12,58 @@
 // TODO:Return the value to function
 
 
-var passLength = prompt ("How long would you like your password?")
-
-
-
-
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate"); {
+function generatePassword () {
 
-  passwordText.value = password;
+  var combinedPass = []
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  var specialCharacters = ["!","@","#","$","%","^","&"]
+  
+  var passLength =0
+  while (passLength < 8 || passLength > 128) {
+    passLength = prompt ("How long would you like your password? (Between 8-128)");
+  }
+  
+    
+  var passUpper = confirm ("Would you like upper case letters?");
+  if (passUpper) {
+    combinedPass+= upperCase
+  }
+  
+  var passLower= confirm ("Would you like lower case letters?");
+  if (passLower) {
+    combinedPass+= lowerCase
+  }
+  var passNum = confirm ("Would you like numbers?");
+  if (passNum) {
+    combinedPass+= numbers
+  }
+  var passChar= confirm ("Would you like special characteres? (!@#$%)");
+  if (passChar) {
+    combinedPass+= specialCharacters
+  }
 
+
+  
+}
+  
+}
+
+
+
+
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
 }
 
 // Add event listener to generate button
